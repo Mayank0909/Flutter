d2abbfac7f575caf4fb3ad446d68c207967587a3
Mayank0909 <mayank10.mv@gmail.com>
2023-02-23 15:18:44 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,81 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("CWRO"),
-      ),
-      body: Container(
-        child: Center(child: Text("Welcome Sourabh")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("CWRO"),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.teal,
+                    gradient:
+                        LinearGradient(colors: [Colors.yellow, Colors.red]),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          offset: Offset(2, 5))
+                    ]),
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.teal,
+                    gradient:
+                        LinearGradient(colors: [Colors.yellow, Colors.red]),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          offset: Offset(2, 5))
+                    ]),
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.teal,
+                    gradient:
+                        LinearGradient(colors: [Colors.yellow, Colors.red]),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          offset: Offset(2, 5))
+                    ]),
+                child: Text(
+                  "Welcome",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
